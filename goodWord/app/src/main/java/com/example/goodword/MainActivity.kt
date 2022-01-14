3c6caff016f7af02b92845aba96c6de417bce237
realibex <ibex97@naver.com>
2022-01-15 00:54:27 +0900

    Complete Project

--- goodWord/app/src/main/java/com/example/goodword/MainActivity.kt
+++ goodWord/app/src/main/java/com/example/goodword/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.goodword
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.goodword.databinding.ActivityMainBinding
 
@@ -13,10 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sentenceList = mutableListOf<String>()
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다2.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다3.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다4.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다5.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다6.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다7.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다8.")
+
+        Log.e("MainActivity",sentenceList.random())
+
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         binding.showAllSentenceBtn.setOnClickListener {
             val intent = Intent(this,SentenceAvtivity::class.java)
             startActivity(intent)
         }
+
+        binding.goodWordTextArea.setText(sentenceList.random())
     }
 }
--- goodWord/app/src/main/java/com/example/goodword/SentenceAvtivity.kt
+++ goodWord/app/src/main/java/com/example/goodword/SentenceAvtivity.kt
@@ -2,10 +2,28 @@ package com.example.goodword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class SentenceAvtivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sentence_avtivity)
+        setContentView(R.layout.activity_sentence)
+
+        val listview = findViewById<ListView>(R.id.sentenceListView)
+
+        val sentenceList = mutableListOf<String>()
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다2.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다3.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다4.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다5.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다6.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다7.")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다8.")
+
+        val sentenceAdater = ListViewAdater(sentenceList)
+        val ListView = findViewById<ListView>(R.id.sentenceListView)
+
+        listview.adapter = sentenceAdater
     }
 }
